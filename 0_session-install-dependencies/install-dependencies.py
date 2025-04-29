f718d34f318e3f06d47bdd8ee60302dcfc2b558a
Adrian Maulana Muhammad <adrian.muhammad@multipolar.com>
2025-04-29 15:07:11 +0700

    first commit

---
+++ 0_session-install-dependencies/install-dependencies.py
@@ -0,0 +1,30 @@
+## This file is provided to the user to accelerate installing dependencies. 
+## Look at sample implementations of this in practice as part of the AMP build script in sample-project-metadata/project-metadata-sample-1.yaml
+
+## Pandas
+!pip install numpy-2.2.5-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
+!pip install python_dateutil-2.9.0.post0-py2.py3-none-any.whl
+!pip install pytz-2025.2-py2.py3-none-any.whl
+!pip install pandas-2.2.3-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
+
+## Streamlit
+!pip install protobuf3-0.2.1.tar.gz
+!pip install click-8.1.8-py3-none-any.whl
+!pip install watchdog-6.0.0-py3-none-manylinux2014_x86_64.whl
+!pip install tornado-6.4.2-cp38-abi3-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl
+!pip install GitPython-3.1.44-py3-none-any.whl 
+!pip install validators-0.34.0-py3-none-any.whl
+!pip install cachetools-5.5.2-py3-none-any.whl
+!pip install typing_extensions-4.13.2-py3-none-any.whl
+!pip install blinker-1.9.0-py3-none-any.whl
+!pip install requests-2.32.3-py3-none-any.whl
+!pip install pygments-2.19.1-py3-none-any.whl
+!pip install mdurl-0.1.2-py3-none-any.whl
+!pip install markdown_it_py-3.0.0-py3-none-any.whl
+!pip install rich-14.0.0-py3-none-any.whl
+!pip install narwhals-1.37.0-py3-none-any.whl
+!pip install altair-5.5.0-py3-none-any.whl
+!pip install tenacity-9.1.2-py3-none-any.whl
+!pip install toml-0.10.2-py2.py3-none-any.whl
+!pip install pydeck-0.9.1-py2.py3-none-any.whl
+!pip install streamlit-1.44.1-py3-none-any.whl
---
+++ README.md
@@ -0,0 +1,4 @@
+# References:
+
+- https://github.com/cloudera/CML_AMP_Streamlit_on_CML/blob/HEAD/.project-metadata.yaml#L9-L10
+- https://github.com/cloudera/CML_Community_AMP_Template/blob/HEAD/3_app-run-python-script/app.py
